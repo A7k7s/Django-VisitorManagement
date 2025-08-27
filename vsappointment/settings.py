4cdd38b8abb32c7175fceba4ff49f772f8654e08
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2025-08-27 19:11:11 +0530

    Update settings.py

--- vsappointment/settings.py
+++ vsappointment/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akshayaapanneerselvam@gmail.com'
-EMAIL_HOST_PASSWORD = 'zqdh swer gzmg cmvr'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MEDIA_URL = '/media/'
